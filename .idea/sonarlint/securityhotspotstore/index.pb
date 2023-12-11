f1d9bc9a118bf1369933b2ef2df92272f54fb952
lina-22 <haquefahinur@gmail.com>
2023-12-11 02:31:33 +0100

    maven project init

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+I
+src/main/webapp/index.jsp,8\e\8e6d3b70c9f5b02570779b0441540d45eceb0ded
+[
++src/main/java/com/exemple/ServeletTest.java,f\f\ff95d6913a14504f12ec2de4ccc5b496ae2fe564
---
+++ src/main/java/com/exemple/ServeletTest.java
@@ -0,0 +1,19 @@
+package com.exemple;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class ServeletTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pr = resp.getWriter();
+        pr.write("its working!!!!");
+    }
+}
+
